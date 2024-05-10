cdb17eb2b1f5e80d70c010a63af6a32cc9820e08
Linh-giang <167959921+Linh-giang@users.noreply.github.com>
2024-05-10 23:22:26 +0700

    Update buoi3.sql

--- buoi3.sql
+++ buoi3.sql
@@ -4,4 +4,16 @@ from students
 where marks > 75
 order by right (name,3), ID ASC
 --baitap2
+select user_id,
+concat(upper(left(name,1)), lower(right(name,length(name)-1))) as name
+from users
+--baitap3
+SELECT 
+manufacturer,
+'$'||round(sum(total_sales)/1000000,0)||' '|| 'million' as sale
+from pharmacy_sales
+group by  manufacturer
+order by sum(total_sales) DESC, manufacturer
+--baitap4
+
                         
