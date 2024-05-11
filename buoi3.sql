66a5f82c89a7d0e3262618a7bf7abadca5d77e82
Linh-giang <167959921+Linh-giang@users.noreply.github.com>
2024-05-11 16:48:40 +0700

    Update buoi3.sql

--- buoi3.sql
+++ buoi3.sql
@@ -15,5 +15,50 @@ from pharmacy_sales
 group by  manufacturer
 order by sum(total_sales) DESC, manufacturer
 --baitap4
+SELECT extract(month from submit_date) as mth,
+product_id,
+round(avg(stars),2)as avg_stars
+from reviews
+group by mth,product_id
+order by mth, product_id
+--baitap5
+SELECT sender_id,
+count(message_id) as message_count
+FROM messages
+where extract(month from sent_date)=8
+and extract(year from sent_date)=2022
+group by sender_id
+order by message_count DESC
+limit 2
+--baitap6
+select tweet_id 
+from tweets
+where length(content)>15
+--baitap7
+  select 
+activity_date as day,
+Count(distinct user_id) as active_users
+from activity
+where activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date
+--baitap8
+select 
+count(employee_id) as count_employee
+from employees
+where extract (month from joining_date) between 1 and 7
+and extract (year from joining_date)=2022
+--baitap9
+select first_name,
+left(right(first_name,2),1)
+from worker
+where first_name='Amitah'
+--baitap10
+select 
+substring (title, length(winery)+2,4)
+from winemag_p2
+where country ='Macedonia'
+
+
+
 
                         
